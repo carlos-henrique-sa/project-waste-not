d4926ba58dd5666e648b3378abc9eb8415fce166
carlos-henrique-sa <carlos.henrique.sa.filho@gmail.com>
2023-12-20 19:52:58 -0300

    initial structure

---
+++ headers.js
@@ -0,0 +1,11 @@
+const headers = [
+    { title: 'Item code', value: 'item_code' },
+    { title: 'Product', value: 'product', sortable: true },
+    { title: 'Package', value: 'package', sortable: true },
+    { title: 'Available units', value: 'available_units', sortable: true },
+    { title: 'Category', value: 'category' },
+    { title: 'Last updated', value: 'category', sortable: true },
+    { title: 'Edit available quantity', value: 'edit_available_quantity' },
+]
+
+export { headers }
