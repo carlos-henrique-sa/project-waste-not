c5578be18123c2ee73dc14d02e18e3039fe961e4
carlos-henrique-sa <carlos.henrique.sa.filho@gmail.com>
2023-12-20 21:18:06 -0300

    all working

--- headers.js
+++ headers.js
@@ -1,10 +1,10 @@
 const headers = [
-    { title: 'Item code', value: 'item_code' },
-    { title: 'Product', value: 'product', sortable: true },
-    { title: 'Package', value: 'package', sortable: true },
-    { title: 'Available units', value: 'available_units', sortable: true },
-    { title: 'Category', value: 'category' },
-    { title: 'Last updated', value: 'category', sortable: true },
+    { title: 'Item code', value: 'item_code', width: '100px' },
+    { title: 'Product', value: 'product', sortable: true, width: '170px' },
+    { title: 'Package', value: 'package', sortable: true, width: '100px'  },
+    { title: 'Available units', value: 'available_units', sortable: true, width: '150px' },
+    { title: 'Category', value: 'category', width: '70px' },
+    { title: 'Last updated', value: 'category', sortable: true, width: '140px' },
     { title: 'Edit available quantity', value: 'edit_available_quantity' },
 ]
 
